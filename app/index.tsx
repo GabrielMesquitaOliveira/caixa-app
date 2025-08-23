31db2c046badc80d933fde8085f4e17867009c30
Gabriel <92613742+GabrielMesquitaOliveira@users.noreply.github.com>
2025-08-23 20:20:35 -0300

    `Added nativewind and tailwindcss to the project, updated dependencies and configuration files accordingly.`

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
     </View>
   );
 }
