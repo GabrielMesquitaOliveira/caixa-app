f5cb3f3a50dbe9e6981a90719b4e3b4caf7b0aac
Gabriel <92613742+GabrielMesquitaOliveira@users.noreply.github.com>
2025-08-25 16:41:34 -0300

    Feat: Fontes da Caixa

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,17 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import React from "react";
 import "./global.css";
+
 export default function RootLayout() {
-    return (
+  const [fontsLoaded] = useFonts({
+    CAIXASTD_Regular: require('../assets/fonts/CAIXAStd-Regular.ttf'),
+    CAIXASTD_Bold: require('../assets/fonts/CAIXAStd-Bold.ttf'),
+    CAIXASTD_Light: require('../assets/fonts/CAIXAStd-Light.ttf'),
+  });
+
+  if (!fontsLoaded) return null;
+  return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#005CA9",
+        secondary: "#F39200",
+      },
+      fontFamily: {
+        sans: ["CAIXASTD_Regular"],  // Fonte padrão
+        bold: ["CAIXASTD_Bold"],
+        light: ["CAIXASTD_Light"],
+      },
+    },
   },
   plugins: [],
 }
